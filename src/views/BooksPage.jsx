3fdfeeffacf529b48e69e51e994cfb787e0c7d61
benhern <43895569+benhern@users.noreply.github.com>
2023-12-01 21:51:01 -0600

    title change

--- src/views/BooksPage.jsx
+++ src/views/BooksPage.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 function BooksPage() {
   const dispatch = useDispatch();
   const books = useSelector(selectBooks).books;
-  const pageTitle = "📖 Book List with Redux and Firebase";
+  const pageTitle = "📖 GitHub Book List with Redux and Firebase";
 
   const bookStatus = useSelector(selectBooks).status;
 
